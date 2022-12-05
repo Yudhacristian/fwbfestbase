905c8c21173b87d184638b856614c510e8f978c1
Yud <101927586+Yudhacristian@users.noreply.github.com>
2022-12-05 17:59:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 load_dotenv("fwb.env")
 
 
-api_id = int(os.environ.get("API_ID", "WAJIB DIISI"))
-api_hash = os.environ.get("API_HASH", "WAJIB DIISI")
-bot_token = os.environ.get("BOT_TOKEN", "WAJIB DIISI")
+api_id = int(os.environ.get("API_ID", "14376741"))
+api_hash = os.environ.get("API_HASH", "8430280fc1476b821e8d3b442c183181")
+bot_token = os.environ.get("BOT_TOKEN", "5894184353:AAEsykfTc4RFh2Y9y2273CtaW-ixqfD6C5E")
 # =========================================================== #
 
-db_url = os.environ.get("DB_URL", "mongodb+srv://username:password@cluster0.d9fwl.mongodb.net/?retryWrites=true&w=majority")
-db_name = os.environ.get("DB_NAME", "telegram")
+db_url = os.environ.get("DB_URL", "mongodb+srv://fwbbase:fwbuniverse@cluster0.bd015lt.mongodb.net/?retryWrites=true&w=majority")
+db_name = os.environ.get("DB_NAME", "fwbbase")
 # =========================================================== #
 
-channel_1 = int(os.environ.get("CHANNEL_1", "WAJIB DIISI"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "WAJIB DIISI"))
+channel_1 = int(os.environ.get("CHANNEL_1", "-1001255135366"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1001846304448"))
 # =========================================================== #
 owner = os.environ.get("OWNER")
-id_admin = int(os.environ.get("ID_ADMIN", "WAJIB DIISI"))
+id_admin = int(os.environ.get("ID_ADMIN", "5039480382"))
 # =========================================================== #
 
 batas_kirim = int(os.environ.get("BATAS_KIRIM", "3"))
@@ -38,7 +38,7 @@ biaya_gfrent = int(os.environ.get("BIAYA_GFRENT", "50"))
 biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "50"))
 # =========================================================== #
 
-hastag = os.environ.get("HASTAG", "#fwbgirl #fwbboy #fwbadlt #fwbspill #fwbstory #ask").replace(" ", "|").lower()
+hastag = os.environ.get("HASTAG", "#ask #adlt #spill #srch #stry #ange").replace(" ", "|").lower()
 # =========================================================== #
 
 pic_boy = os.environ.get("PIC_BOY")
