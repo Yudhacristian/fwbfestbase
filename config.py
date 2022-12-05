b2102d7a91be076dd6e7f566e2d3b07fb145047b
Yud <101927586+Yudhacristian@users.noreply.github.com>
2022-12-05 12:39:54 +0700

    config.py

--- config.py
+++ config.py
@@ -20,22 +20,22 @@ owner = os.environ.get("OWNER")
 id_admin = int(os.environ.get("ID_ADMIN", "WAJIB DIISI"))
 # =========================================================== #
 
-batas_kirim = int(os.environ.get("BATAS_KIRIM", "5"))
-batas_talent = int(os.environ.get("BATAS_TALENT", "10"))
-batas_daddy_sugar = int(os.environ.get("BATAS_DADDY_SUGAR", "10"))
-batas_moansgirl = int(os.environ.get("BATAS_MOANSGIRL", "10"))
-batas_moansboy = int(os.environ.get("BATAS_MOANSBOY", "10"))
-batas_gfrent = int(os.environ.get("BATAS_GFRENT", "10"))
-batas_bfrent = int(os.environ.get("BATAS_BFRENT", "10"))
+batas_kirim = int(os.environ.get("BATAS_KIRIM", "3"))
+batas_talent = int(os.environ.get("BATAS_TALENT", "50"))
+batas_daddy_sugar = int(os.environ.get("BATAS_DADDY_SUGAR", "20"))
+batas_moansgirl = int(os.environ.get("BATAS_MOANSGIRL", "20"))
+batas_moansboy = int(os.environ.get("BATAS_MOANSBOY", "20"))
+batas_gfrent = int(os.environ.get("BATAS_GFRENT", "20"))
+batas_bfrent = int(os.environ.get("BATAS_BFRENT", "20"))
 # =========================================================== #
 
-biaya_kirim = int(os.environ.get("BIAYA_KIRIM", "10"))
-biaya_talent = int(os.environ.get("BIAYA_TALENT", "80"))
-biaya_daddy_sugar = int(os.environ.get("BIAYA_DADDY_SUGAR", "70"))
-biaya_moansgirl = int(os.environ.get("BIAYA_MOANSGIRL", "60"))
+biaya_kirim = int(os.environ.get("BIAYA_KIRIM", "20"))
+biaya_talent = int(os.environ.get("BIAYA_TALENT", "100"))
+biaya_daddy_sugar = int(os.environ.get("BIAYA_DADDY_SUGAR", "50"))
+biaya_moansgirl = int(os.environ.get("BIAYA_MOANSGIRL", "50"))
 biaya_moansboy = int(os.environ.get("BIAYA_MOANSBOY", "50"))
-biaya_gfrent = int(os.environ.get("BIAYA_GFRENT", "40"))
-biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "30"))
+biaya_gfrent = int(os.environ.get("BIAYA_GFRENT", "50"))
+biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "50"))
 # =========================================================== #
 
 hastag = os.environ.get("HASTAG", "#fwbgirl #fwbboy #fwbadlt #fwbspill #fwbstory #ask").replace(" ", "|").lower()
@@ -48,7 +48,7 @@ start_img = os.environ.get("START_IMG")
 group = os.environ.get("GROUP")
 pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat diakses harap join terlebih dahulu")
 
-gagalkirim_msg = os.environ.get("GAGAL_KIRIM", "{mention}, pesan mu gagal terkirim silahkan gunakan hastag:\n\n#fwbgirl Untuk cewe\n#fwbboy Untuk cowo\n#fwbadlt Untuk 18+\n#fwbspill Untuk pamer\n#fwbstory Untuk bercerita / curhat\n#ask Untuk bertanya\n\n🗣️ Wajib username ya fewbess")
+gagalkirim_msg = os.environ.get("GAGAL_KIRIM", "{mention}, pesan mu gagal terkirim silahkan klik /start lalu pilih menu menfes")
 
 menu_msg = os.environ.get("PESAN_MENU", "#fwbgirl Untuk cewe\n#fwbboy Untuk cowo\n#fwbadlt Untuk 18+\n#fwbspill Untuk pamer\n#fwbstory Untuk bercerita / curhat\n#ask Untuk bertanya\n\n🗣️ Wajib username ya fewbess")
 
