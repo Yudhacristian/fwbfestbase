b0b9c39fc702deb3d254803c9b3f702447d386cf
TeamKillerX <killerx@randydev.my.id>
2022-12-06 12:25:38 +0700

    Add files via upload

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,12 +1,2 @@
-from .awal_handler import *
-from .tambah_admin import *
-from .send_handler import * 
-from .ban_handler import *
-from .bot_handler import *
-from .talent_handler import *
-from .daddy_sugar import *
-from .moans_girl import *
-from .moans_boy import *
-from .boyfriend_rent import *
-from .girlfriend_rent import *
-from .broadcast_handler import *
+from .helper import Helper, Waktu
+from .database import Database
